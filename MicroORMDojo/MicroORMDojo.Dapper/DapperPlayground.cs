2c59cea58b1a00d65b3d4fb26557626d575b0790
Mateusz Bilinski - QIAGEN <Mateusz.Bilinski@qiagen.com>
2017-06-07 10:06:35 +0200

    Added SQL for creating Northwind and solution with project for dapper

---
+++ MicroORMDojo/MicroORMDojo.Dapper/DapperPlayground.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace MicroORMDojo.Dapper
+{
+    class DapperPlayground
+    {
+        private static IDbConnection GetSqlConnection()
+        {
+            return new SqlConnection(ConfigurationManager.ConnectionStrings["Northwind"].ConnectionString);
+        }
+
+        static void Main(string[] args)
+        {
+            var playground = new DapperPlayground();
+
+            using (var conn = GetSqlConnection())
+            {
+                // here comes the...
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
