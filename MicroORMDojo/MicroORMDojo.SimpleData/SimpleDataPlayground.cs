a5bc06706b42fdbbdaf2ce37dae0749f3760100c
Mateusz Bilinski - QIAGEN <Mateusz.Bilinski@qiagen.com>
2017-06-07 21:52:33 +0200

    Added project for Simple.Data

---
+++ MicroORMDojo/MicroORMDojo.SimpleData/SimpleDataPlayground.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Simple.Data;
+
+namespace MicroORMDojo.SimpleData
+{
+    class SimpleDataPlayground
+    {
+        private static dynamic GetSqlConnection()
+        {
+            return Database.OpenNamedConnection("Northwind");
+        }
+
+        static void Main(string[] args)
+        {
+            var playground = new SimpleDataPlayground();
+
+            using (var db = GetSqlConnection())
+            {
+                playground.DoFunnyThings(db);
+            }
+
+            Console.ReadKey();
+        }
+
+        public void DoFunnyThings(dynamic db)
+        {
+            // here comes the...
+        }
+    }
+}
